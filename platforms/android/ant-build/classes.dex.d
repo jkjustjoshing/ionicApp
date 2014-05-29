d2ced29dad3b7d0d80a693cd0240f6d967ecad81
Josh Kramer <josh@joshkra.me>
2014-05-28 22:28:50 -0400

    Initialize codebase

---
+++ platforms/android/ant-build/classes.dex.d
@@ -0,0 +1,12 @@
+/Users/joshkramer/Documents/Development/ionicApp/platforms/android/ant-build/classes.dex : \
+/Users/joshkramer/Documents/Development/ionicApp/platforms/android/ant-build/classes/com/ionic/keyboard/IonicKeyboard$1.class \
+/Users/joshkramer/Documents/Development/ionicApp/platforms/android/ant-build/classes/com/ionic/keyboard/IonicKeyboard.class \
+/Users/joshkramer/Documents/Development/ionicApp/platforms/android/ant-build/classes/com/ionicframework/starter/BuildConfig.class \
+/Users/joshkramer/Documents/Development/ionicApp/platforms/android/ant-build/classes/com/ionicframework/starter/HelloCordova.class \
+/Users/joshkramer/Documents/Development/ionicApp/platforms/android/ant-build/classes/com/ionicframework/starter/R$attr.class \
+/Users/joshkramer/Documents/Development/ionicApp/platforms/android/ant-build/classes/com/ionicframework/starter/R$drawable.class \
+/Users/joshkramer/Documents/Development/ionicApp/platforms/android/ant-build/classes/com/ionicframework/starter/R$string.class \
+/Users/joshkramer/Documents/Development/ionicApp/platforms/android/ant-build/classes/com/ionicframework/starter/R$xml.class \
+/Users/joshkramer/Documents/Development/ionicApp/platforms/android/ant-build/classes/com/ionicframework/starter/R.class \
+/Users/joshkramer/Documents/Development/ionicApp/platforms/android/ant-build/classes/org/apache/cordova/device/Device.class \
+/Users/joshkramer/Documents/Development/ionicApp/platforms/android/ant-build/dexedLibs/classes-13f4f812cceed81df236ee587a397cf2.jar \
